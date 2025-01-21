a0608152aad188dc60bca28c9cece9cd54fbacd6
Mikhalib Green <mikhalibgreen@gmail.com>
2025-01-20 21:54:05 -0600

    Fixed Miscallaneous Pluggins and Keymappings
    + Added gitignore
    - Removed lazy-lock.json
    + Added lazy-lock.json to .gitignore
    + Fixed miscallaneous pluggins.

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,5 +1,40 @@
 return {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" }
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		require("harpoon").setup()
+
+		-- set keymaps
+		local harpoon = require("harpoon")
+		local keymap = vim.keymap
+
+		keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end)
+		keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end)
+
+		keymap.set("n", "<C-j>", function()
+			harpoon:list():select(1)
+		end)
+		keymap.set("n", "<C-k>", function()
+			harpoon:list():select(2)
+		end)
+		keymap.set("n", "<C-k>", function()
+			harpoon:list():select(3)
+		end)
+		keymap.set("n", "<C-l>", function()
+			harpoon:list():select(4)
+		end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		keymap.set("n", "<C-S-,>", function()
+			harpoon:list():prev()
+		end)
+		keymap.set("n", "<C-S-.>", function()
+			harpoon:list():next()
+		end)
+	end,
 }
