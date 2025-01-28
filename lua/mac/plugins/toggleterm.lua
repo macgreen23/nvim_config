6bce02ea6ea5f648ef6e75e402a7f36f41dae30b
Mikhalib Green <crazyclock02@gmail.com>
2025-01-28 12:15:11 -0600

    Added vimtex
    * Fixed tab for nvim-cmp
    * Fixed terminal for toggle term
    * Added vimtex

--- lua/mac/plugins/toggleterm.lua
+++ lua/mac/plugins/toggleterm.lua
@@ -1,17 +1,26 @@
 return {
 	"akinsho/toggleterm.nvim",
-	opts = {
-		winbar = {
-			enabled = true,
-			name_formatter = function(term)
-				return term.name
-			end,
-		},
-	},
 	config = function()
+		local toggleterm = require("toggleterm")
+
+		toggleterm.setup({
+			winbar = {
+				enabled = true,
+				name_formatter = function(term)
+					return term.name
+				end,
+			},
+		})
 		-- Set keymaps
 		local keymap = vim.keymap
 
 		keymap.set("n", "<leader>y", "<cmd>ToggleTerm<CR>", { desc = "Toggles Terminal" })
+		keymap.set("t", "<esc>", [[<C-\><C-n>]])
+		keymap.set("t", "jk", [[<C-\><C-n>]])
+		keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]])
+		keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]])
+		keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]])
+		keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]])
+		keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]])
 	end,
 }
