102a9be2c6e823f849e06771bcae688f03a904c7
Mikhalib Green <mikhalibgreen@gmail.com>
2025-06-10 11:36:32 -0500

    More refactoring

---
+++ after/queries/python/injections.scm
@@ -0,0 +1,4 @@
+; extends
+; Docstrings
+((expression_statement (string (string_content) @injection.content))
+ (#set! injection.language "rst"))
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -16,6 +16,7 @@ return {
     auto_install = true, -- automatically install syntax support when entering new file type buffer
     ensure_installed = {
       'lua',
+      'python'
     },
   },
   config = function (_, opts)
